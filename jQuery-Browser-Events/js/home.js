ab5dbac4a79acb1814a45604beaf09136b45468e
KennethMelendez <kmlndz221@gmail.com>
2018-03-04 21:11:22 -0500

    fixing up button toggle

--- jQuery-Browser-Events/js/home.js
+++ jQuery-Browser-Events/js/home.js
@@ -1,61 +1,97 @@
-//Bug using booleans so created an integer for button presses
-let buttonPressed = 1;
+
+$(document).ready(function () {
+  $("#mainInfoDiv").show();
+});
+
+/*=================================================================
+
+Created an algorithm that automaticly toggles
+and displays content on the page
+  FOR MAIN DISPLAY
+Directions:
+    Align the content array with the button array. 
+       content and button should correspond
+        with the same index in order to work.
+
+=================================================================*/
+
 
 //ARRAY OF CONTENT
 let hiddenContent = [
-                     "#mainInfoDiv",
-                     "#akronInfoDiv",
-                     "#minneapolisInfoDiv",
-                     "#minneapolisWeather",
-                     "#louisvilleInfoDiv",
-                     "#louisvilleWeather",
-                    ];
+  "#mainInfoDiv",
+  "#akronInfoDiv",
+  "#minneapolisInfoDiv",
+  "#louisvilleInfoDiv",
+];
 //ARRAY OF BUTTONS
 let buttons = [
-                     "#mainButton",
-                     "#akronButton",
-                     "#minneapolisButton",
-                     "#minneapolisWeather",
-                     "#louisvilleButton",
-                     "#louisvilleWeatherButton",
-                    ];
+  "#mainButton",
+  "#akronButton",
+  "#minneapolisButton",
+  "#louisvilleButton",
+];
+
+//ARRAY OF PARAGRAPH CONTENT
+let paragraphContent = [
+  "#minneapolisWeather",
+  "#louisvilleWeather",
+  "#akronWeather"
+];
+
+//ARRAY OF PARAGRAPHBUTTONS
+let paragraphButtons = [
+  "#minneapolisWeatherButton",
+  "#louisvilleWeatherButton",
+  "#akronWeatherButton"
+];
+
+
 
 
 hideContent();
-
-
-$(document).ready(function(){
-$("#mainInfoDiv").show();
-});
-
-
-function hideContent(){
-for(let x = 0; x < hiddenContent.length;x++){
-  $(hiddenContent[x]).hide();
-  console.log(hiddenContent[x]);
-  } // end of for loop
-}
-
 hideContent();
 buttonClick();
+activateParagraphButtons();
 
-function buttonClick(){
+function activateParagraphButtons() {
+  for (let content = 0; content < paragraphButtons.length; content++) {
+    let currentButton = paragraphButtons[content];
+    let currentContent = paragraphContent[content];
+    togglesParagraphButtons(currentButton,currentContent);
+  }
+
+}
 
 
-for(let showContent = 0; showContent < hiddenContent.length;showContent++){
-  for(let hideContent = 0; hideContent < hiddenContent.length; hideContent++){
-  $(buttons[showContent]).on("click",function(){
+
+function hideContent() {
+  for (let x = 0; x < hiddenContent.length; x++) {
+    $(hiddenContent[x]).hide();
+    console.log(hiddenContent[x]);
+  }
+}
+
+function togglesParagraphButtons(userButton, userContent) {
+  $(userContent).hide();
+  $(userButton).on("click", function () {
+    $(userContent).toggle();
+  })
+}
+
+
+
+function buttonClick() {
+
+  for (let showContent = 0; showContent < hiddenContent.length; showContent++) {
+    for (let hideContent = 0; hideContent < hiddenContent.length; hideContent++) {
+      $(buttons[showContent]).on("click", function () {
 
 
         $(hiddenContent[hideContent]).hide();
         $(hiddenContent[showContent]).show();
 
 
-  });
-}
-}
-
-
-
-
+      });
+    }
+  }
 }
